3fd59985db12a918e5dd5de197dce926748a75d8
akkipaul2000 <skypaul2000@gmail.com>
2024-05-05 21:37:25 +0530

    a little updates on submition succesfull

--- leet_recur.js
+++ leet_recur.js
@@ -1,8 +1,24 @@
-var minCostClimbingStairs = function(cost) {
-    cost.push(0)
-     for(let i=cost.length-4;i>=0;i--){
-         cost[i]+=Math.min(cost[i+1],cost[i+2])
-     }
-       return Math.min(cost[0],cost[1])
+// var minCostClimbingStairs = function(cost) {
+//     cost.push(0)
+//      for(let i=cost.length-4;i>=0;i--){
+//          cost[i]+=Math.min(cost[i+1],cost[i+2])
+//      }
+//        return Math.min(cost[0],cost[1])
  
- };
+//  };
+
+function minCostClimbingStairs(cost) {
+    let n = cost.length;
+    let first = cost[0];
+    let second = cost[1];
+    if (n <= 2) return Math.min(first, second);
+    for (let i = 2; i < n; i++) {
+        console.log("Before Swapping",first,second,cost[i])
+        let curr = cost[i] + Math.min(first, second);
+        first = second;
+        second = curr;
+        console.log("After Swapping Stuffs",first,second)
+    }
+    // return Math.min(first, second);
+}
+minCostClimbingStairs([1,100,1,1,1,100,1,1,100,1])
